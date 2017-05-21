50681700063ce148d983e7dbb50c5efdd4d1ad37
Pandaklez <belkannkl@gmail.com>
2017-05-21 13:54:10 +0300

    Add files via upload

--- something like a website/app/views.py
+++ something like a website/app/views.py
@@ -1,42 +1,86 @@
 from flask import render_template, redirect, url_for, request, flash
-from app import app, db, models
-from .models import *
+from app import app, db
 
 import json
 import os.path
 import re
+import csv
+import sqlite3
+
+def get_wordcol():
+    con = sqlite3.connect("app.db")
+    cur = con.cursor()
+    cur.execute('SELECT word FROM t')
+    result = cur.fetchall()
+    return result
 
 @app.route('/')
 def home():
-    x = url_for('map')
     tabl = url_for('table')
-    return render_template('base.html',
+    stat = url_for('stat')
+    home = url_for('home')
+
+    return render_template('home.html',
                            table=tabl,
-                           mapp=x)
+                           home=home,
+                           stat=stat)
+
+@app.route('/stat')
+def stat():
+    tabl = url_for('table')
+    stat = url_for('stat')
+    home = url_for('home')
+    
+    return render_template('stat.html',
+                           home=home,
+                           table=tabl,
+                           stat=stat)
+
+#def making_pages(html):
+#    for el in html:
+#        route = '/map/' + el
+@app.route('/map/<word>')
+def map():
+    con = sqlite3.connect("app.db")
+    cur = con.cursor()
+    cur.execute('SELECT word FROM t')
+    words = cur.fetchall()
+    for word in words:
+        return render_template(word + '.html',
+                               title='Map')
 
 @app.route('/table')
 def table():
     tabl = url_for('table')
-    x = url_for('map')
-    
-    first_table = models.Base.query.all()
-    second_table = models.Languages.query.all()
+    stat = url_for('stat')
 
-#    mapp = models.Base.query.filter_by(word='bicycle').first().template
-#    x = url_for('map')
-    return render_template('table.html',
-                           title='Table',
-                           base=first_table,
-                           lang=second_table,
-                           table=tabl)
-@app.route('/map')
-def map():
-    tabl = url_for('table')
-    x = url_for('map')
+    result = get_wordcol()
+    arr = []
+    for element in result:
+        z = element[0]
+        if z not in arr:
+            arr.append(z)
+    #global html
+    html = []
+    for e in arr:
+        p = e + ".html"
+        html.append(p)
+
+    #making_pages(html)
+        
+    tb = []
+    i = 0
+    for n in html:
+        k = url_for("map/<html[i]>")
+        tb.append(k)
+        i += 1
     
-    mapp = models.Base.query.filter_by(word='bicycle').first().template
-    #'\'' + mapp + '\''
-    return render_template('bicycle.html',
-                           title='Map',
-                           mapp=x,
-                           table=tabl)
+    return render_template('db_table.html',
+                           title='Table',
+                           tb=tb,
+                           home=home,
+                           table=tabl,
+                           stat=stat,
+                           words=arr)
+
+
